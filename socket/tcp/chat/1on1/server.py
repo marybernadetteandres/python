64c1a03e29e908c5994967dbfb48b5be75a7739d
tanaka0079 <yadoran001@yahoo.co.jp>
2015-09-01 22:47:20 +0900

    Update server.py

--- socket/tcp/chat/1on1/server.py
+++ socket/tcp/chat/1on1/server.py
@@ -9,10 +9,10 @@ def main():
     print("Conneted by"+str(addr))  #サーバ側の合図
 
     while (1):
-        data = raw_input("Server&gt;") # 入力待機(サーバー側)
+        data = raw_input("Server>") # 入力待機(サーバー側)
         soc.send(data)              # ソケットにデータを送信
         data = soc.recv(1024)       # データを受信（1024バイトまで）
-        print "Client&gt;",data        # サーバー側の書き込みを表示
+        print "Client>",data        # サーバー側の書き込みを表示
         if data == "q":             # qが押されたら終了
             soc.close()
             break
