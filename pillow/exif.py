f101b5f581fa66bb5bad342233d24c8d8714612c
tanaka0079 <yadoran001@yahoo.co.jp>
2015-09-03 02:38:39 +0900

    Update exif.py

--- pillow/exif.py
+++ pillow/exif.py
@@ -3,11 +3,11 @@ from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
 
 def main():
-    get_type ="DateTimeOriginal"
+    get_type ="DateTimeOriginal"    # 入手したいExifのデータ種類
     exif_data = []
-    im = Image.open("test.jpg")
-    exif = im._getexif()
-
+    im = Image.open("test.jpg")     # 画像の取得
+    exif = im._getexif()            # 画像からExifデータを抽出
+    # Exifデータから特定のデータのみ抽出
     for id, value in exif.items():
         if TAGS.get(id) == get_type:
             tag = TAGS.get(id, id),value
