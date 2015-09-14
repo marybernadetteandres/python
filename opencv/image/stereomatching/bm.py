9e9128b6a6fdb8e1904c825a9a2065676d389548
tanaka0079 <yadoran001@yahoo.co.jp>
2015-09-14 16:44:39 +0900

    Update and rename opencv/stereo_bm.py to opencv/image/stereomatching/bm.py

--- opencv/image/stereomatching/bm.py
+++ opencv/image/stereomatching/bm.py
@@ -5,9 +5,6 @@ def main():
     # 画像取得
     gray_l = cv2.imread("left.png",0)
     gray_r = cv2.imread("right.png",0)
-    # 画像のヒストグラム平坦化・平滑化
-    gray_l = cv2.GaussianBlur( cv2.equalizeHist(gray_l),(5,5), 0)
-    gray_r = cv2.GaussianBlur( cv2.equalizeHist(gray_r),(5,5), 0)
     # BM法でステレオ対応点探索
     stereo = cv2.StereoBM(cv2.STEREO_BM_BASIC_PRESET,ndisparities=32, SADWindowSize=21)
     disp = stereo.compute(gray_l,gray_r)    # 視差を計算
