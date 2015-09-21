63009e2eda577d6d0b6b7bd442bc7d87b1b21bab
tanaka0079 <yadoran001@yahoo.co.jp>
2015-09-21 12:23:49 +0900

    Update meanshift.py

--- opencv/image/segmentation/meanshift.py
+++ opencv/image/segmentation/meanshift.py
@@ -3,8 +3,8 @@ import cv2
 
 def main():
     im = cv2.imread("test.jpg")                     # 画像の読み込み
-    cv2.pyrMeanShiftFiltering(im, 20, 200, im, 2)   # MeanShiftで領域分割
-    cv2.imshow("Keypoint",im)                       # ウィンドウの表示
+    cv2.pyrMeanShiftFiltering(im, 2, 30, im, 1)     # MeanShiftで領域分割
+    cv2.imshow("Show",im)                           # ウィンドウの表示
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
