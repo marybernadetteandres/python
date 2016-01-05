79fa2d03a65ac4fc79ff3914d5788bb166cecee9
tanaka0079 <yadoran001@yahoo.co.jp>
2016-01-05 13:41:46 +0900

    Update labeling.py

--- opencv/labeling.py
+++ opencv/labeling.py
@@ -1,19 +1,31 @@
 # -*- coding: utf-8 -*-
 import cv2
+import numpy as np
 
 def main():
-    # 画像取得
-    im = cv2.imread("test.png")                 # 入力画像の取得
-    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) # 入力画像をグレースケール変換
-    th = cv2.threshold(gray,127,255,0)[1]       # グレースケール画像の2値化
-    # 輪郭抽出
-    cnts = cv2.findContours(th,1,2)[0]          # グレースケール画像から輪郭検出
-    cv2.drawContours(im,cnts,-2,(255,0,0),-1)   # 最大の輪郭を描く
-    # 結果表示
-    cv2.imshow("Show Image",im)                 # 画像の表示
-    cv2.waitKey(0)                              # キー入力待機
-    cv2.destroyAllWindows()                     # ウィンドウ破棄
+    # 入力画像の取得
+    im = cv2.imread("test.jpg")
+    # グレースケール変換
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    # 2値化
+    gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    # ラベリング処理
+    n, label = cv2.connectedComponents(gray)
+    # ラベルの個数分だけ色を用意
+    rgbs = np.random.randint(0,255,(n+1,3))
 
+    # ラベル付けした各マスクを色付け
+    for y in xrange(0, gray.shape[0]):
+        for x in xrange(0, gray.shape[1]):
+            if label[y, x] > 0:
+                im[y, x] = rgbs[label[y, x]]
+            else:
+                im[y, x] = [0, 0, 0]
 
-if __name__ == "__main__":
+    # ウィンドウ表示
+    cv2.imshow("Labeling", im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
     main()
